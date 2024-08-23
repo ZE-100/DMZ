065c23bf77d313e36e70b6bc7e816c5875b69618
Z-100 <marvin131t@gmail.com>
2024-08-23 20:36:42 +0200

    feature(#5): Add scanners and make pgsql work

--- dmz-app/src/main/kotlin/ch/zindustries/dmz/app/DmzApplication.kt
+++ dmz-app/src/main/kotlin/ch/zindustries/dmz/app/DmzApplication.kt
@@ -1,8 +1,10 @@
 package ch.zindustries.dmz.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication(
     scanBasePackages = ["ch.zindustries.dmz"],
@@ -10,6 +12,8 @@ import org.springframework.boot.runApplication
         SecurityAutoConfiguration::class,
     ]
 )
+@EntityScan("ch.zindustries.dmz")
+@EnableJpaRepositories("ch.zindustries.dmz")
 open class DmzAppApplication
 
 fun main(args: Array<String>) {
